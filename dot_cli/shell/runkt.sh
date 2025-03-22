53ef597993138734fc3535fb81acc832b8c13e56
widehyo@gmail.com <widehyo@gmail.com>
2025-03-23 00:34:16 +0900

    add: buffer_fzf.vim WIP

---
+++ dot_cli/shell/runkt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kt_file_name=${1##*/}
+jar_file_name=${kt_file_name/.kt/.jar}
+echo kotlinc $1 -include-runtime -d $jar_file_name && echo java -jar $jar_file_name
+kotlinc $1 -include-runtime -d $jar_file_name  && java -jar $jar_file_name
