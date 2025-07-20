3a602c9f813c3e69679a73fc38ade2bf017fbf59
widehyo@gmail.com <widehyo@gmail.com>
2025-07-20 22:12:28 +0900

    shell

---
+++ dot_cli/shell/apply_nvimconf_from_dot.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+echo find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | cut -c50- | tar -czvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/gitclone/dot_files/dot_config/nvim/ --files-from -
+find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | cut -c50- | tar -czvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/gitclone/dot_files/dot_config/nvim/ --files-from -
+echo tar -xzvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim
+tar -xzvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim
+echo rm /home/widehyo/.config/nvim/nvimconf.tgz
+rm /home/widehyo/.config/nvim/nvimconf.tgz
