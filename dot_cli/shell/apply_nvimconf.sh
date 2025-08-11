f05e272d86752ec84d2b870a6598540982b88014
widehyo@mobigen.com <widehyo@mobigen.com>
2025-08-11 10:57:15 +0900

    fix: modify apply nvimconf shell(delete insert)

--- dot_cli/shell/apply_nvimconf.sh
+++ dot_cli/shell/apply_nvimconf.sh
@@ -1,4 +1,8 @@
-!#/bin/bash
+#!/bin/bash
+echo find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | xargs -I@ rm @
+find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | xargs -I@ rm @
+echo find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | cut -c28- | tar -czvf /home/widehyo/gitclone/dot_files/dot_config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim/ --files-from -
+find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | cut -c28- | tar -czvf /home/widehyo/gitclone/dot_files/dot_config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim/ --files-from -
 echo find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | cut -c28- | tar -czvf /home/widehyo/gitclone/dot_files/dot_config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim/ --files-from -
 find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | cut -c28- | tar -czvf /home/widehyo/gitclone/dot_files/dot_config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim/ --files-from -
 echo tar -xzvf /home/widehyo/gitclone/dot_files/dot_config/nvim/nvimconf.tgz --directory /home/widehyo/gitclone/dot_files/dot_config/nvim
--- dot_cli/shell/apply_nvimconf_from_dot.sh
+++ dot_cli/shell/apply_nvimconf_from_dot.sh
@@ -1,4 +1,6 @@
-!#/bin/bash
+#!/bin/bash
+echo find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | xargs -I@ rm @
+find /home/widehyo/.config/nvim -type f | grep -E ".lua$|.vim$" | xargs -I@ rm @
 echo find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | cut -c50- | tar -czvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/gitclone/dot_files/dot_config/nvim/ --files-from -
 find /home/widehyo/gitclone/dot_files/dot_config/nvim -type f | grep -E ".lua$|.vim$" | cut -c50- | tar -czvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/gitclone/dot_files/dot_config/nvim/ --files-from -
 echo tar -xzvf /home/widehyo/.config/nvim/nvimconf.tgz --directory /home/widehyo/.config/nvim
