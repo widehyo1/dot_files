833c7e75e000f98f21db8f1ca4e1252067a2470e
widehyo@mobigen.com <widehyo@mobigen.com>
2025-08-11 09:35:58 +0900

    execute lua

--- dot_config/nvim/ftplugin/lua.lua
+++ dot_config/nvim/ftplugin/lua.lua
@@ -1,4 +1,8 @@
 local bufnr = vim.api.nvim_get_current_buf()
+local b_local = { buffer = 0 }
+
 vim.bo[bufnr].tabstop = 2
 vim.bo[bufnr].shiftwidth = 2
 vim.bo[bufnr].softtabstop = 2
+
+vim.keymap.set('n', '<leader>rr', ':source %<CR>', b_local)
