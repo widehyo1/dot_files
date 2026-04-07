f63a3d2d6cf17926cb930a5cc137d4dce97c460d
widehyo@gmail.com <widehyo@gmail.com>
2026-04-07 22:50:47 +0900

    fix: install.sh using tar

--- install.sh
+++ install.sh
@@ -6,19 +6,22 @@ BASH_CONFIG_DIR="$HOME/.config/bash"
 SECURITYENV_FILE="$BASH_CONFIG_DIR/securityenv"
 SECURITYENV_BAK="$BASH_CONFIG_DIR/securityenv_bak"
 
-echo cp -r "$DOT_FILES_DIR"/dot_vim/* ~/.vim
-cp -r "$DOT_FILES_DIR"/dot_vim/* ~/.vim
+mkdir -p ~/.vim
+echo tar -C "$DOT_FILES_DIR"/dot_vim -cf - . | tar -C ~/.vim -xf -
+tar -C "$DOT_FILES_DIR"/dot_vim -cf - . | tar -C ~/.vim -xf -
 echo cp "$DOT_FILES_DIR"/dot_vimrc ~/.vimrc
 cp "$DOT_FILES_DIR"/dot_vimrc ~/.vimrc
 echo cp "$DOT_FILES_DIR"/dot_inputrc ~/.inputrc
 cp "$DOT_FILES_DIR"/dot_inputrc ~/.inputrc
 echo cp "$DOT_FILES_DIR"/dot_gitconfig ~/.gitconfig
 cp "$DOT_FILES_DIR"/dot_gitconfig ~/.gitconfig
-echo cp -r "$DOT_FILES_DIR"/dot_cli/* ~/.cli
-cp -r "$DOT_FILES_DIR"/dot_cli/* ~/.cli
+mkdir -p ~/.cli
+echo tar -C "$DOT_FILES_DIR"/dot_cli -cf - . | tar -C ~/.cli -xf -
+tar -C "$DOT_FILES_DIR"/dot_cli -cf - . | tar -C ~/.cli -xf -
 
-echo cp -r "$DOT_FILES_DIR"/dot_config/bash/* "$BASH_CONFIG_DIR"
-cp -r "$DOT_FILES_DIR"/dot_config/bash/* "$BASH_CONFIG_DIR"
+mkdir -p "$BASH_CONFIG_DIR" 
+echo tar -C "$DOT_FILES_DIR"/dot_config/bash -cf - . | tar -C "$BASH_CONFIG_DIR" -xf -
+tar -C "$DOT_FILES_DIR"/dot_config/bash -cf - . | tar -C "$BASH_CONFIG_DIR" -xf -
 echo cp ~/.bashrc ~/.bashrc_bak
 cp ~/.bashrc ~/.bashrc_bak
 echo cp "$SECURITYENV_FILE" "$SECURITYENV_BAK"
