0eb248bda193ffc2c5fa2f62957acc2ff5bfa06c
widehyo@gmail.com <widehyo@gmail.com>
2026-04-12 01:49:24 +0900

    add: install with stow

--- install_with_stow.sh
+++ install_with_stow.sh
@@ -7,8 +7,8 @@ SECURITYENV_FILE="$BASH_CONFIG_DIR/securityenv"
 SECURITYENV_BAK="$BASH_CONFIG_DIR/securityenv_bak"
 
 mkdir -p ~/.vim
-echo 'tar -C "$DOT_FILES_DIR"/dot_vim -cf - . | tar -C ~/.vim -xf -'
-tar -C "$DOT_FILES_DIR"/dot_vim -cf - . | tar -C ~/.vim -xf -
+echo stow dot_vim -t ~/.vim --adopt
+stow dot_vim -t ~/.vim --adopt
 echo cp "$DOT_FILES_DIR"/dot_vimrc ~/.vimrc
 cp "$DOT_FILES_DIR"/dot_vimrc ~/.vimrc
 echo cp "$DOT_FILES_DIR"/dot_inputrc ~/.inputrc
@@ -16,12 +16,12 @@ cp "$DOT_FILES_DIR"/dot_inputrc ~/.inputrc
 echo cp "$DOT_FILES_DIR"/dot_gitconfig ~/.gitconfig
 cp "$DOT_FILES_DIR"/dot_gitconfig ~/.gitconfig
 mkdir -p ~/.cli
-echo 'tar -C "$DOT_FILES_DIR"/dot_cli -cf - . | tar -C ~/.cli -xf -'
-tar -C "$DOT_FILES_DIR"/dot_cli -cf - . | tar -C ~/.cli -xf -
+echo stow dot_cli -t ~/.cli --adopt
+stow dot_cli -t ~/.cli --adopt
 
 mkdir -p "$BASH_CONFIG_DIR" 
-echo 'tar -C "$DOT_FILES_DIR"/dot_config/bash -cf - . | tar -C "$BASH_CONFIG_DIR" -xf -'
-tar -C "$DOT_FILES_DIR"/dot_config/bash -cf - . | tar -C "$BASH_CONFIG_DIR" -xf -
+echo 'cd "$DOT_FILES_DIR"/dot_config && stow bash -t "$BASH_CONFIG_DIR" --adopt'
+cd "$DOT_FILES_DIR"/dot_config && stow bash -t "$BASH_CONFIG_DIR" --adopt
 echo cp ~/.bashrc ~/.bashrc_bak
 cp ~/.bashrc ~/.bashrc_bak
 echo cp "$SECURITYENV_FILE" "$SECURITYENV_BAK"
